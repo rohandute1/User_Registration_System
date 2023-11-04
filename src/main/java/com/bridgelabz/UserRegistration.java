990741307be0ce087e51cef13016b9f4021ac0bf
Rohan <rohandute1@gmail.com>
2023-11-05 01:56:17 +0530

    Added:- successfully added functionality which validate first name in user registration

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,14 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public String validateFirstName() {
-        return "Welcome to User Registration System";
+    final static String regex = "^[A-Z]{1}[a-z]{3,}$";
+
+    public static boolean validateFirstName(String firstName) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(firstName);
+        return match.matches();
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
     UserRegistration userRegistration = new UserRegistration();
+
+    public UserRegistrationTest() {
+    }
+
     @Test
     public void testValidFirstNameWithMinimumLength(){
-        userRegistration.validateFirstName();
-        String result = userRegistration.validateFirstName();
-        Assertions.assertEquals("Welcome to User Registration System",result);
+        boolean res = UserRegistration.validateFirstName("Rohan");
+        Assertions.assertTrue(res);
     }
 }
