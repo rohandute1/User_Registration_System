88de1db667959d902852c9556b67fd858524f132
Rohan <rohandute1@gmail.com>
2023-11-19 13:31:29 +0530

    Added:- implemented exception to user registration problem

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,38 +1,28 @@
 package com.bridgelabz;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class UserRegistration {
-    final static String regex = "^[A-Z]{1}[a-z]{3,}$";
-    final static String regexID = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$";
-    final static String regexNumber = "^91 *[789]{1}[0-9]{9}$";
-    final static String regexPassword = "^(?![.-])[A-Za-z0-9]+(?<!\\.)@[A-Za-z]+(\\.[A-Za-z]+)+$";
-
-    public static boolean validateFirstName(String firstName) {
-        Pattern pattern = Pattern.compile(regex);
-        Matcher match = pattern.matcher(firstName);
-        return match.matches();
+    public static boolean validateFirstName(String firstName) throws IllegalArgumentException{
+        if (firstName.matches("^[A-Z][a-z]{3,}$")){
+            return true;
+        } else throw new IllegalArgumentException("Invalid first name");
     }
-    public static boolean validateLastName(String lastName){
-        Pattern pattern = Pattern.compile(regex);
-        Matcher match = pattern.matcher(lastName);
-        return match.matches();
+    public static boolean validateLastName(String lastName) throws IllegalArgumentException{
+        if (lastName.matches("^[A-Z][a-z]{3,}$")){
+            return true;
+        } else throw new IllegalArgumentException("Invalid last name");
     }
-    public static boolean validateEmailID(String email){
-        Pattern pattern = Pattern.compile(regexID);
-        Matcher match = pattern.matcher(email);
-        return match.matches();
+    public static boolean validateEmailID(String email) throws IllegalArgumentException{
+        if (email.matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")){
+            return true;
+        } else throw new IllegalArgumentException("Invalid email address");
     }
-    public static boolean validatePhoneNumber(String number){
-        Pattern pattern = Pattern.compile(regexNumber);
-        Matcher match = pattern.matcher(number);
-        return match.matches();
+    public static boolean validatePhoneNumber(String number) throws IllegalArgumentException{
+        if (number.matches("^91 *[789][0-9]{9}$")){
+            return true;
+        } else throw new IllegalArgumentException("Invalid phone number");
     }
-
-    public static boolean validatePassword(String pass) {
-        Pattern pattern = Pattern.compile(regexPassword);
-        Matcher match  = pattern.matcher(pass);
-        return match.matches();
+    public static boolean validatePassword(String password) throws IllegalArgumentException{
+        if (password.matches("^(?![.-])[A-Za-z0-9]+(?<!\\.)@[A-Za-z]+(\\.[A-Za-z]+)+$")){
+            return true;
+        } else throw new IllegalArgumentException("Invalid password");
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -6,15 +6,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class UserRegistrationTest {
-    UserRegistration userRegistration = new UserRegistration();
 
     public UserRegistrationTest() {
     }
 
     @Test
     public void testValidFirstNameWithMinimumLength(){
-        boolean res = UserRegistration.validateFirstName("Rohan");
-        Assertions.assertTrue(res);
+        try {
+            boolean res = UserRegistration.validateFirstName("Rohan");
+            Assertions.assertTrue(res);
+        } catch (IllegalArgumentException e){
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
     }
     @ParameterizedTest
     @CsvSource({
@@ -23,13 +26,21 @@ public class UserRegistrationTest {
             "Ro,false"
     })
     public void testInvalidFirstNameWithMinimumLength(String firstName,boolean expected){
-        boolean res = UserRegistration.validateFirstName(firstName);
-        Assertions.assertEquals(expected,res);
+        try{
+            boolean res = UserRegistration.validateFirstName(firstName);
+            Assertions.assertEquals(expected,res);
+        } catch (IllegalArgumentException e){
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
     }
     @Test
     public void testValidLastNameWithMinimumLength(){
-        boolean res = UserRegistration.validateLastName("Dute");
-        Assertions.assertTrue(res);
+        try {
+            boolean res = UserRegistration.validateLastName("Dute");
+            Assertions.assertTrue(res);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
     }
     @ParameterizedTest
     @CsvSource({
@@ -37,13 +48,22 @@ public class UserRegistrationTest {
             "psjdbg,false",
     })
     public void testInvalidLastNameWithMinimumLength(String lastName,boolean expected){
-        boolean res = UserRegistration.validateLastName(lastName);
-        Assertions.assertEquals(expected,res);
+        try{
+            boolean res = UserRegistration.validateLastName(lastName);
+            Assertions.assertEquals(expected,res);
+        } catch (IllegalArgumentException e){
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
+
     }
     @Test
     public void testGivenEmailIDValidOrNot(){
-        boolean res = UserRegistration.validateEmailID("abc@yahoo.com");
-        Assertions.assertTrue(res);
+        try{
+            boolean res = UserRegistration.validateEmailID("abc@yahoo.com");
+            Assertions.assertTrue(res);
+        } catch (IllegalArgumentException e){
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
     }
     @ParameterizedTest
     @CsvSource({
@@ -62,8 +82,13 @@ public class UserRegistrationTest {
             "abc@gmail.com.aa.au, true"
     })
     public void testGivenInValidEmails(String email, boolean expected){
-        boolean res = UserRegistration.validateEmailID(email);
-        Assertions.assertEquals(res,expected);
+        try {
+            boolean res = UserRegistration.validateEmailID(email);
+            Assertions.assertEquals(res,expected);
+        }catch (IllegalArgumentException e){
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
+
     }
     @ParameterizedTest
     @CsvSource({
@@ -72,13 +97,23 @@ public class UserRegistrationTest {
             "925223452852, false"
     })
     public void testGivenPhoneNumberInValid(String number, boolean expected){
-        boolean res = UserRegistration.validatePhoneNumber(number);
-        Assertions.assertEquals(res, expected);
+        try {
+            boolean res = UserRegistration.validatePhoneNumber(number);
+            Assertions.assertEquals(res, expected);
+        } catch (IllegalArgumentException e){
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
+
     }
     @Test
     public void testGivenPasswordAndReturnValidOrNot(){
-        boolean res = UserRegistration.validatePassword("RohnDute@1");
-        Assertions.assertTrue(res);
+        try {
+            boolean res = UserRegistration.validatePassword("RohnDute@1");
+            Assertions.assertTrue(res);
+        } catch (IllegalArgumentException e){
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
+
     }
     @ParameterizedTest
     @CsvSource({
@@ -87,7 +122,12 @@ public class UserRegistrationTest {
             "123@Rohan, false"
     })
     public void testInValidPassword(String pass, boolean expected){
-        boolean res = UserRegistration.validatePassword(pass);
-        Assertions.assertEquals(res, expected);
+        try {
+            boolean res = UserRegistration.validatePassword(pass);
+            Assertions.assertEquals(res, expected);
+        } catch (IllegalArgumentException e){
+            System.out.println("Unexpected exception: " + e.getMessage());
+        }
+
     }
 }
